ab44e403dcbf7ac33a6fbedfe704a1dd3191a163
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-06-10 10:05:07 +0200

    added prettier and also render countries

--- App.js
+++ App.js
@@ -3,24 +3,14 @@ import { StyleSheet, Text, View, Image, FlatList } from 'react-native'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import axios from 'axios'
-import adam from './assets/adam.png'
 
 export default function App() {
   const [data, setData] = useState([])
-  // const [searchCountry, setSearchCountry] = useState('')
 
   const getData = async () => {
     const { data } = await axios.get(`https://restcountries.com/v3.1/all`)
-    //Random 200 countries
-    const totalShownCountries = 200
-    const picked = []
-    for (let i = 0; i < totalShownCountries; i++) {
-      const index = Math.floor(Math.random() * data.length)
-      const removed = data.splice(index, 1)
-      picked.push(removed[0])
-    }
-    setData(picked)
-    console.log('Visible countries', picked)
+
+    setData(data)
   }
   useEffect(() => {
     getData()
@@ -28,15 +18,6 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Image source={adam}></Image>
-      <Text
-        style={{
-          color: 'springgreen',
-          fontSize: 60
-        }}
-      >
-        HEJ ADAM
-      </Text>
       <StatusBar style="auto" />
       <FlatList
         data={data}
@@ -53,7 +34,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 100,
     backgroundColor: 'hotpink',
     alignItems: 'center',
     justifyContent: 'center'
