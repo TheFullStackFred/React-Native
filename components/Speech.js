f8a0fdc10256429c5be21b4b2893ae4c13c358a1
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-06-14 21:24:40 +0200

    mhhm

--- components/Speech.js
+++ components/Speech.js
@@ -4,18 +4,14 @@ import * as Speech from 'expo-speech'
 
 export default function App() {
   const speak = () => {
-    const thingToSay =
+    const speechText =
       'Asså snälla fan tackat nej, tackat nej mannen ah mannen stick härifrån vafan mannen tackat nej tackar du nej nån annanstans, mannen tackat nej fattar du den där mannen get the f out of here, asså åh vadå tackat nej, jag tackar nej mannen vadå tackat nej. Du sa! Tacka nej!'
-    Speech.speak(thingToSay)
+    Speech.speak(speechText)
   }
 
   return (
-    <View style={{ color: 'green' }}>
-      <Button
-        style={{ color: 'red' }}
-        title="This is Expo-Speech hit me to try"
-        onPress={speak}
-      />
+    <View>
+      <Button title="This is Expo-Speech hit me to try" onPress={speak} />
     </View>
   )
 }
